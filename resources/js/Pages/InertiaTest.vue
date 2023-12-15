1ad85d6a2a460114bd0dfe35d9eae73e952f3988
HarukiSugimoto1021 <h.sugimoto.howcollect@gmail.com>
2023-12-15 13:47:33 +0900

    inertia-vue.js

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue' // リアクティブ
+import { Link } from '@inertiajs/inertia-vue3';
+const newTitle = ref('')
+const newContent = ref('')
+</script>
+
+<template>
+    Inertiaテストです。<br>
+    <a href="/">aタグ経由です</a><br>
+    <Link href="/">Link経由です</Link><br>
+    <Link :href="route('inertia.index')">Indexへ</Link><br>
+    <Link :href="route('inertia.show', {id: 1})">ルートパラメータテストです</Link>
+    <div class="mb-8"></div>
+    <input type="text" name="newTitle" v-model="newTitle">{{ newTitle }}<br>
+    <input type="text" name="newContent" v-model="newContent">{{ newContent }}<br>
+
+    <Link as="button" method="post" :href="route('inertia.store')" :data="{ title:newTitle, content: newContent}">保存テスト</Link>
+</template>
