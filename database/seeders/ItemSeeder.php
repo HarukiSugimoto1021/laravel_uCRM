47ff910f07e8467207f00d16316918334cef61b7
HarukiSugimoto1021 <h.sugimoto.howcollect@gmail.com>
2023-12-18 14:15:07 +0900

    商品関連

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,6 +16,16 @@ class ItemSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('items')->insert([
+            [
+                'name' => 'カット', 'memo' => 'カットの詳細', 'price' => 6000
+            ],
+            [
+                'name' => 'カラー', 'memo' => 'カラーの詳細', 'price' => 8000
+            ],
+            [
+                'name' => 'パーマ(カット込)', 'memo' => 'パーマ、カットの詳細', 'price' => 13000
+            ],
+            ]);
     }
 }
